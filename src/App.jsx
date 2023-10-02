44487f475fe975ac5ab8ea9217980827264cf924
FrigoAlex <alejo012903@hotmail.com>
2023-10-02 01:07:43 -0500

    💄 Add style to the page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import PokeCard from "./components/PokeCard";
 import "./App.css";
 
 const App = () => {
-
   return (
-    <div className="page-container">
-      <PokeCard/>
+    <div className="container mx-auto py-10">
+      <h1 className="text-center text-4xl font-bold uppercase">Pokedex</h1>
+      <PokeCard />
     </div>
   );
 };
