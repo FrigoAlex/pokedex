acdb98fd7c7d9e92d9065a89fadee3507e4cb224
FrigoAlex <alejo012903@hotmail.com>
2023-10-02 01:06:49 -0500

    🧐 Test a new pokemon request

--- src/hooks/useMons.jsx
+++ src/hooks/useMons.jsx
@@ -4,7 +4,9 @@ import { useState, useEffect } from "react";
 const useMons = () => {
   const [mons, setMons] = useState([]);
   const getPokedata = async () => {
-  const { data } = await axios.get("https://pokeapi.co/api/v2/pokedex/2/");
+    const { data } = await axios.get("https://pokeapi.co/api/v2/pokedex/1/");
+
+    console.log(data.pokemon_entries.url);
 
     setMons(data?.pokemon_entries);
   };
